2a7a22e1d57e88b4d0f8b6b70b79b9abd6aa291f
Yegor Golovach <77448464+AlintaJP@users.noreply.github.com>
2022-09-16 18:02:45 +0300

    FormField component fixes

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { UseFormRegisterReturn } from "react-hook-form/dist/types/form";
 
 interface Props {
   placeholder: string;
   type?: string;
   label: string;
   textarea?: boolean;
+  register: UseFormRegisterReturn;
 }
 
-const FormField = ({ placeholder, type, label, textarea }: Props) => {
+const FormField = ({ placeholder, type, label, textarea, register }: Props) => {
   const className = `shadow border rounded py-2 px-3 ${
     textarea ? "form-textarea" : "form-input"
   } mt-1 block w-full ring-yellow-500 outline-none focus:ring`;
@@ -16,9 +18,15 @@ const FormField = ({ placeholder, type, label, textarea }: Props) => {
     <label className="block mb-5">
       <span className="text-gray-700">{label}</span>
       {textarea ? (
-        <textarea className={className} rows={8} placeholder={placeholder} />
+        <textarea
+          className={className}
+          {...register}
+          rows={8}
+          placeholder={placeholder}
+        />
       ) : (
         <input
+          {...register}
           className={className}
           type={type || "text"}
           placeholder={placeholder}
